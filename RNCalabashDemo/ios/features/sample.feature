20a152718a8113aa07669be855b83f047c1e9848
Florian Rival <Florian.Rival@gmail.com>
2016-04-05 00:47:54 +0200

    Add a simple test

--- RNCalabashDemo/ios/features/sample.feature
+++ RNCalabashDemo/ios/features/sample.feature
@@ -2,6 +2,4 @@ Feature: Sample Feature
 
 Scenario: Sample Scenario
   Given the app has launched
-  And I have done a specific thing
-  When I do something
-  Then something should happen
+  Then I should see "Welcome to React Native!"
--- RNCalabashDemo/ios/features/steps/sample_steps.rb
+++ RNCalabashDemo/ios/features/steps/sample_steps.rb
@@ -4,42 +4,8 @@ Given(/^the app has launched$/) do
   end
 end
 
-And(/^I have done a specific thing$/) do
-  # Example: Given I am logged in
-  #  wait_for do
-  #    !query("* marked:'username'").empty?
-  #  end
-  #
-  #  touch("* marked:'username'")
-  #  wait_for_keyboard
-  #  keyboard_enter_text("cleveruser27")
-  #
-  #  touch("* marked:'password'")
-  #  wait_for_keyboard
-  #  keyboard_enter_text("pa$$w0rd")
-  #
-  #  wait_for_element_exists("* marked:'Login'")
-  #  touch("* marked:'Login'")
-
-  # Remember: any Ruby is allowed in your step definitions
-  did_something = true
-
-  unless did_something
-    fail 'Expected to have done something'
+Then(/^I should see '(.*)'$/) do |expected_mark|
+  until element_exists("view marked:'#{expected_mark}'") || element_exists("view text:'#{expected_mark}'")
+    screenshot_and_raise "No element found with mark or text: #{expected_mark}"
   end
 end
-
-When(/^I do something$/) do
-  # Example: When I create a new entry
-  #  tap("* marked:'new_entry'")
-  #  wait_for_keyboard
-  #  keyboard_enter_text("* marked:'entry_title'", 'My Entry')
-  #
-  #  tap("* marked:'submit'")
-end
-
-Then(/^something should happen$/) do
-  # Example: Then I should see the entry on my home page
-  #  wait_for_element_exists("* text:'My Entry'")
-end
-
